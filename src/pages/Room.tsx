987bbf9a9e9af906fef83ffbeaa3b8e237f36f15
bfsgr <bfsgr.code@protonmail.com>
2021-06-27 22:33:22 -0300

    move

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,7 +2,7 @@ import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 
 import '../styles/room.scss';
-import { RoomCode } from '../contexts/RoomCode';
+import { RoomCode } from '../components/RoomCode';
 import { useParams } from 'react-router-dom';
 import { FormEvent, useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
